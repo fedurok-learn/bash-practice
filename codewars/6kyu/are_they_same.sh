cf399d1dcbbda8d003b06be392b35a94bf63d2c0
Nikolay Fedurko <32279961+B1Z0N@users.noreply.github.com>
2021-02-11 22:57:14 +0300

    shellcheck

--- codewars/6kyu/are_they_same.sh
+++ codewars/6kyu/are_they_same.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -c
+#!/bin/bash
 
 # https://www.codewars.com/kata/550498447451fbbd7600041c/train/shell
 # parameters are strings; return "true" or "false" (see Sample Tests)
@@ -13,17 +13,16 @@ sort_spaced() {
 square() {
   for val in $1
   do
-    echo $(($val ** 2))
+    echo $((val ** 2))
   done | paste -sd ' '
 }
 
 comp() {
-  asorted="$(sort_spaced $1)"
+  asorted=$(sort_spaced "$1")
   asquared=$(square "$asorted")
-  bsorted="$(sort_spaced $2)"
+  bsorted=$(sort_spaced "$2")
 
   [ "$asquared" = "$bsorted" ] && echo true || echo false
 }
 
 comp "$1" "$2"
-
